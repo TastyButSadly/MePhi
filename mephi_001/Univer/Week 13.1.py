f9c4d7bafbfaf10e3441df0fa8dc9fde58e4ebfa
TastyButSadly <toflash004@gmail.com>
2023-12-20 23:24:10 +0300

    Fist attempt

--- mephi_001/Univer/Week 13.1.py
+++ mephi_001/Univer/Week 13.1.py
@@ -1,17 +1,38 @@
 import matplotlib.pyplot as plt
+import csv
 import numpy as np
-import pandas as pd
+
+X = []
+Y = []
+X_2 = []
 
 
-data = np.array()
+def plot_data(file_path):
+    with open(file_path, 'r') as datafile:
+        data = csv.reader(datafile, delimiter=' ')
 
-x = data[:, 0]
-I = data[:, 1]
+        for row in data:
+            #print(row[0])
+            arr_num = row[0].split(' ')
+            # print(arr_num)
+            # print(float(arr_num[0]) ** 2, arr_num[-1])
+            X.append(float(arr_num[0]) ** 2)
+            Y.append(float(arr_num[-1]))
+            # X_2.append(float(arr_num[0]))
+    return [X, Y]
 
 
-plt.plot(x, I, marker='o', linestyle='-', color='b')
-plt.title('График зависимости момента инерции от расстояния')
-plt.xlabel('Расстояние, м')
-plt.ylabel('Момент инерции')
-plt.grid(True)
+# print(X, '\n', Y))
+X, Y = plot_data('inertia_2.25_0.43.csv')
+plt.plot(X, Y)
+coeff = np.polyfit(X, Y, 1)
+print(f"собственный момент инерции: {round(coeff[0], 6)}")
+xn = np.linspace(X[1], X[-1], 2)
+yn = np.poly1d(coeff)
+plt.plot(xn, yn(xn), X, Y)
+# plt.plot(X_2, Y)
+plt.title('Line graph')
+# plt.xlabel('X')
+# plt.ylabel('Y')
 plt.show()
+
